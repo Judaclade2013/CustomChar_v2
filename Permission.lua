90c9e0868663dbe76a7bd0ea962dcc0d0ca2047b
Judaclade2013 <161789685+Judaclade2013@users.noreply.github.com>
2024-03-07 19:54:50 -0300

    Update Permission.lua

--- Permission.lua
+++ Permission.lua
@@ -1,46 +1,28 @@
-	local Tabela = {
+local Tabela = {
 	PlayerPermission = {
-		["KozierNoob123"] = 			{Tier = 5}, -- oi
-		["Fonojonzo"] = 			{Tier = 1000},
-		["ExTnoCraK"] = 			{Tier = 1000},
-		["VigorOGr"] = 				{Tier = 5},
-		["jguuh"] = 				{Tier = 5},
+		["Judaclade2013"] = 			{Tier = 2000},
+		["ExTnoCraK"] = 			{Tier = 2000},
+		["VigorOGr"] = 				{Tier = 1000},
+		["jguuh"] = 				{Tier = 1000},
 		["b112014"] = 				{Tier = 1},
-		["ynxvmp"] = 				{Tier = 1},
+		["ProzinhoBr23213"] = 			{Tier = 1},
 		["zazax1120"] = 			{Tier = 1},
-		["ProzinhoBr23213"] = 			{Tier = 1},		
-		["Renan2809"] = 			{Tier = 777777},
-		["NuelNoCra"] = 			{Tier = 2},
-		["Biscoituuh"] = 			{Tier = 5},
-		["GG_TIAGO100"] = 			{Tier = 1},
-		["FerPro_13"] = 			{Tier = 1},
-		["FaustinoCB"] = 			{Tier = 1},
-		["ltz_Lufe"] = 				{Tier = 1}, -- c
-		["srnubaso"] = 				{Tier = 1}, -- nubaseo
-		["dizesetigueimer"] = 			{Tier = 999999}, -- nubaseo
 	},
 	
 	CharacterTier = {
-		["gojo"] 	= 				{Tier = 1,Icon = 8088264406,Permission = {36651812,4386076823,1525050995,2908376678,2386263216,375521456,
-				4776221194,1204916120,375521456,1502674877,4035850120,1513291266,3667599085,4645701612,3947573158,3760549720,3251265663,
-				2948781812,3337736146,2402884749,2885875741,5021357221,5359593504,2747160102,3322067390,1905649594,3756477172,2438209670,
-				4826162014, -- matando de hollow purple. aviso 1/3
-				2468759752, --helper do mês 01/03/24	
-			}},		
-		["sukuna"]	=				{Tier = 1000,Icon = 16208424020,Locked = {36651812}},
-		["goku"]	=				{Locked = {36651812,128677359,785068132},Icon = 14181223410},
-		["jp6"]		=				{Tier = 2,Icon = 13166084061,Permission = {36651812,2386263216,128677359,834610807,1793600541,834610807,3162059849,3108673472,4700176285}},
-		["rimuru"]	=				{Tier = 5,Icon = 15124481761,Permission = {3337736146,4992950422}},			
-		["nakime"]	=				{Locked = {785068132,3108673472}},
-		["gyutaro"]	=				{Locked = {785068132}},
-		["gojov2"]	=				{Locked = {36651812,128677359},Icon = 16136393866},
-		["teengojo"]	=				{Locked = {36651812,128677359},Icon = 14023532241},
-		["okuyasu"]	=				{Tier = 5,Permission = {36651812,834610807,75422217},Icon = 1974417519},
-		["pain"]	=				{Tier = 1,Permission = {36651812,4035850120,3337736146,1513291266,2008992958},Icon = 11539866072},
-		["diavolo"]	=				{Tier = 5,Permission = {36651812,3207378051},Icon = 6861041809},
-		["giorno"]	=				{Tier = 5,Permission = {36651812,3108673472,3667599085},Icon = 15193933291},
-		["itachi"]	=				{Tier = 5,Permission = {36651812,3667599085,1209522522,1502674877,5551458487,128677359,3207378051,785068132},Icon = 25444441},
-		["mahoraga"]	=				{Tier = 99999,Locked = {36651812,785068132},Icon = 15759260117},
+		["Enrico"] = 					{Tier = 1000,Permission = {3128682307}},
+		["DIO"]	=					{Locked = {3128682307}},
+		["Madara"]	=				{Locked = {36651812,785068132}},
+		["Koku"]	=				{Locked = {785068132}},
+		["Yori"]	=				{Permission = {36651812,3108673472}},
+		["Meliodas"]	=				{Locked = {3128682307}},
+		["Goku"]	=				{Locked = {785068132}},
+		["TOP"]	=					{Locked = {785068132}},
+		["Giorno"] = 					{Tier = 1050,Permission = {36651812,785068132,3108673472,834610807}},
+		["Hakaishin"] = 				{Locked = {36651812,785068132,629259254}},
+		["Boss"] = 					{Locked = {785068132,36651812}},
+		["JP6"] = 					{Tier = 1,Permission = {3128682307,2908376678,1706565385,3108673472,3162059849}},
+		["Gojo"] = 					{Tier = 1,Permission = {3128682307,2963030044}}, -- perm temp
 	}
 
 }
